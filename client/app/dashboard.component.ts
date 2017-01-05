c5afcf846c18f9cddada31a3ced4ea93cb60234d
Mathias Bragagia <mathias.bragagia@epitech.eu>
2017-01-05 09:06:39 +0100

    Adds dashboard

--- client/app/dashboard.component.ts
+++ client/app/dashboard.component.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { Card } from './card';
+import { CardService } from './card.service';
 
 @Component({
+	moduleId: module.id,
 	selector: 'my-dashboard',
-	template: `<h3>My Dashboard</h3>`
+	templateUrl: `dashboard.component.html`
 })
 export class DashboardComponent {
+	cards: Card[];
 
+	constructor(private cardService: CardService) { }
+
+	ngOnInit(): void {
+		this.cardService.getCards().then(cards => this.cards = cards.slice(1, 5));
+	}
 }
